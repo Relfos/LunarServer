a3b9f613547dbfc944f34c8df545fa66cc6b3b7d
Sergio Flores <relfos@gmail.com>
2017-11-10 03:11:01 +0000

    Added EntityAPI

---
+++ SynkServer/Entity/API.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using LunarParser;
+using SynkServer.Core;
+using SynkServer.HTTP;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SynkServer.Entity
+{
+    public class APIRequestException : Exception
+    {
+
+        public APIRequestException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class EntityAPI: SitePlugin
+    {
+        private DataFormat format;
+        private string mimeType;
+
+        private string rootPath;
+
+        public EntityAPI(DataFormat format = DataFormat.JSON)
+        {
+            this.format = format;
+            this.mimeType = "application/" + format.ToString().ToLower();
+        }
+
+        public override bool Install(Site site, string path)
+        {
+            this.site = site;
+            this.rootPath = path;
+            return true;
+        }
+
+        protected HTTPResponse HandleRequest(HTTPRequest request)
+        {
+            var url = Utils.FixUrl(request.path);
+
+            if (url.Equals("/favicon.ico"))
+            {
+                return null;
+            }
+
+            DataNode result;
+            var query = request.args;
+
+            RouteEntry route = null;// router.Find(url, query);
+
+            if (route != null)
+            {
+                var handler = route.handler;
+
+                try
+                {
+                    result = (DataNode) handler(request);
+                }
+                catch (Exception ex)
+                {
+                    result = OperationResult(ex.Message);
+                }
+
+            }
+            else
+            {
+                result = DataNode.CreateObject("result");
+                result.AddField("error", "invalid route");
+            }
+
+            var response = new HTTPResponse();
+            
+
+            var content = DataFormats.SaveToString(format, result);
+            response.bytes = System.Text.Encoding.UTF8.GetBytes(content);
+
+            response.headers["Content-Type"] = mimeType;
+
+            return response;
+            
+        }
+
+        public static DataNode OperationResult(string errorMsg = null)
+        {
+            var result = DataNode.CreateObject("operation");
+            result.AddField("result", string.IsNullOrEmpty(errorMsg) ? "success" : "error");
+            if (!string.IsNullOrEmpty(errorMsg))
+            {
+                result.AddField("error", errorMsg);
+            }
+            return result;
+        }
+
+        public void GenerateAPI<T>() where T : Entity
+        {
+            var name = typeof(T).Name.ToLower() + "s";
+            var baseURL = Combine(rootPath, $"api/{name}");
+            site.Get(baseURL, (request) => ListEntities<T>());
+            site.Get(baseURL + "/new", (request) => NewEntity<T>(request));
+            site.Get(baseURL + "/{id}/show", (request) => ShowEntity<T>(request));
+            site.Get(baseURL + "/{id}/edit", (request) => EditEntity<T>(request));
+            site.Get(baseURL + "/{id}/delete", (request) => DeleteEntity<T>(request));
+        }
+
+        private static void Check(Dictionary<string, string> dic, IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                if (!dic.ContainsKey(arg))
+                {
+                    throw new APIRequestException($"argument '{arg}' is required");
+                }
+            }
+        }
+
+        private static string Check(Dictionary<string, string> dic, string name)
+        {
+            if (dic.ContainsKey(name))
+            {
+                return dic[name];
+            }
+
+            throw new APIRequestException($"argument '{name}' is required");
+        }
+
+        private static T Check<T>(string id) where T : Entity
+        {
+            var entity = Entity.Find<T>(id);
+
+            if (entity != null)
+            {
+                return entity;
+            }
+
+            throw new APIRequestException($"{typeof(T).Name} with '{id}' does not exist");
+        }
+
+        #region UTILS
+        private static object ListEntities<T>() where T : Entity
+        {
+            var result = DataNode.CreateObject(typeof(T).Name.ToLower() + "s");
+
+            foreach (var user in Entity.List<T>())
+            {
+                var data = user.Serialize();
+                result.AddNode(data);
+            }
+
+            return result;
+        }
+
+        private static object ShowEntity<T>(HTTPRequest requset) where T : Entity
+       {
+            var id = requset.args["id"];
+            var entity = Entity.Find<T>(id);
+            var data = entity.Serialize();
+            return data;
+        }
+
+        private static object NewEntity<T>(HTTPRequest request) where T : Entity
+        {
+            var entity = Entity.Create<T>();
+            var node = request.args.ToDataSource(typeof(T).Name.ToLower());
+            Check(request.args, Entity.GetFields<T>());
+            entity.Deserialize(node);
+            entity.Save();
+            var data = entity.Serialize();
+            return data;
+        }
+
+        private static object EditEntity<T>(HTTPRequest request) where T : Entity
+        {
+            var node = request.args.ToDataSource(typeof(T).Name.ToLower());
+            var id = Check(request.args, "id");
+            var entity = Check<T>(id);
+            entity.Deserialize(node);
+            entity.Save();
+            var data = entity.Serialize();
+            return data;
+        }
+
+        private static object DeleteEntity<T>(HTTPRequest request) where T : Entity
+        {
+            var node = request.args.ToDataSource(typeof(T).Name.ToLower());
+            var id = Check(request.args, "id");
+            var entity = Check<T>(id);
+            entity.Delete();
+            return OperationResult();
+        }
+
+        #endregion
+
+    }
+}
---
+++ SynkServer/Entity/Entity.cs
@@ -0,0 +1,250 @@
+﻿using LunarParser;
+using LunarParser.XML;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace SynkServer.Entity
+{
+    public class EntityCollection
+    {
+        public Type objectType { get; private set;}
+
+        public EntityCollection(Type objectType)
+        {
+            this.objectType = objectType;
+
+            if (File.Exists(GetFileName()))
+            {
+                var root = XMLReader.ReadFromString(File.ReadAllText(GetFileName()));
+                var name = objectType.Name.ToLower();
+
+                root = root["collection"];
+
+                foreach (var child in root.Children)
+                {
+                    if (!child.Name.Equals(name))
+                    {
+                        continue;
+                    }
+
+                    var obj = AllocObject();
+                    obj.Deserialize(child);
+                    _objects[obj.ID] = obj;
+                }
+            }
+        }
+
+        private string GetFileName()
+        {
+            return objectType.Name + ".xml";
+        }
+
+        private Dictionary<string, Entity> _objects = new Dictionary<string, Entity>();
+        public IEnumerable<Entity> Objects { get { return _objects.Values; } }
+
+        private Entity AllocObject()
+        {
+            lock (this)
+            {
+                var obj = (Entity)Activator.CreateInstance(objectType);
+                return obj;
+            }
+        }
+
+        public Entity CreateObject()
+        {
+            var obj = AllocObject();
+            obj.ID = Guid.NewGuid().ToString();
+            _objects[obj.ID] = obj;
+            return obj;
+        }
+
+        public Entity FindObject(string ID)
+        {
+            lock (this)
+            {
+                if (_objects.ContainsKey(ID))
+                {
+                    return _objects[ID];
+                }
+                return null;
+            }
+        }
+
+        public void DeleteObject(string ID)
+        {
+            if (_objects.ContainsKey(ID))
+            {
+                _objects.Remove(ID);
+            }
+        }
+
+        public void Save()
+        {
+            lock (this)
+            {
+                var result = DataNode.CreateObject("collection");
+                foreach (var obj in _objects.Values)
+                {
+                    var node = obj.Serialize();
+                    result.AddNode(node);
+                }
+
+                var contents = XMLWriter.WriteToString(result);
+                File.WriteAllText(GetFileName(), contents);
+                _shouldSave = false;
+            }
+        }
+
+        private bool _shouldSave = false;
+
+        public void RequestSave()
+        {
+            lock (this)
+            {
+                _shouldSave = true;
+            }
+        }
+    }
+
+    public abstract class Entity
+    {
+        public string ID;
+
+        private static Dictionary<Type, EntityCollection> _collections = new Dictionary<Type, EntityCollection>();
+
+        private static Thread saveThread = null;
+
+        public static T Create<T>() where T : Entity
+        {
+            var type = typeof(T);
+            var collection = GetCollection(type);
+            return (T)collection.CreateObject();
+        }
+
+        private static EntityCollection GetCollection(Type type)
+        {
+            EntityCollection collection;
+
+            if (_collections.ContainsKey(type))
+            {
+                collection = _collections[type];
+            }
+            else
+            {
+                collection = new EntityCollection(type);
+                _collections[type] = collection;
+            }
+
+            return collection;
+        }
+
+        public static T Find<T>(string ID) where T : Entity
+        {
+            var type = typeof(T);
+
+            var collection = GetCollection(type);
+            return (T)_collections[type].FindObject(ID);
+        }
+
+        public void Delete()
+        {
+            var type = this.GetType();
+            var collection = GetCollection(type);
+            collection.DeleteObject(this.ID);
+            RequestBackgroundThread();
+        }
+
+        public void Save()
+        {
+            var type = this.GetType();
+            var collection = GetCollection(type);
+            collection.RequestSave();            
+            RequestBackgroundThread();
+        }
+
+        private void RequestBackgroundThread()
+        {
+            if (saveThread == null)
+            {
+                saveThread = new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+
+                    do
+                    {
+                        Thread.Sleep(500);
+                        foreach (var collection in _collections.Values)
+                        {
+                            collection.Save();
+                        }
+                    } while (true);
+                });
+
+                saveThread.Start();
+            }
+        }
+
+        public static IEnumerable<String> GetFields<T>() where T: Entity
+        {
+            Type type = typeof(T);
+            return type.GetFields().Where(f => f.IsPublic).Select(x => x.Name);
+        }
+
+        public DataNode Serialize()
+        {
+            Type type = this.GetType();
+            var result = DataNode.CreateObject(type.Name.ToLower());
+
+            var fields = type.GetFields().Where(f => f.IsPublic);
+            foreach (var field in fields)
+            {
+                var val = field.GetValue(this);
+                if (val != null)
+                {
+                    result.AddField(field.Name, val.ToString());
+                }
+            }
+
+            return result;
+        }
+
+        public void Deserialize(DataNode node)
+        {
+            Type type = this.GetType();
+
+            var fields = type.GetFields().Where(f => f.IsPublic);
+            foreach (var field in fields)
+            {
+                if (!node.HasNode(field.Name))
+                {
+                    continue;
+                }
+
+                var val = node.GetString(field.Name);
+
+                object obj = val;
+                
+                field.SetValue(this, obj);                
+            }
+        }
+
+        public static IEnumerable<T> List<T>() where T:Entity
+        {
+            var type = typeof(T);
+            var result = new List<T>();
+
+            var collection = GetCollection(type);
+
+            foreach (var obj in collection.Objects)
+            {
+                result.Add((T)obj);
+            }
+            
+            return result;
+        }
+    }
+}
