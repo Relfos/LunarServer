bfdbd9a04b19e1bac2af7fc49906751610d29b2b
Sergio Flores <relfos@gmail.com>
2023-03-11 22:35:42 +0000

    Added RequestExtensions

---
+++ LunarServer/Utils/RequestExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LunarLabs.WebServer.HTTP;
+
+namespace LunarLabs.WebServer.Utils
+{
+    public static class RequestExtensions
+    {
+        public static long GetLong(this HTTPRequest request, string name)
+        {
+            var str = request.GetVariable(name);
+            long result;
+            if (long.TryParse(str, out result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+
+        public static T GetEnum<T> (this HTTPRequest request, string name) where T: struct
+        {
+            var str = request.GetVariable(name);
+
+            var type = typeof(T);
+            T result;
+
+            if (Enum.TryParse<T>(str, out result))
+            {
+                return (T)result;
+            }
+
+            return default(T);
+        }
+    }
+}
